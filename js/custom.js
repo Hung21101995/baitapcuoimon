38fb799c85e6add7e61a6c0f8b9994c1fe56be6d
Hung21101995 <97971362+Hung21101995@users.noreply.github.com>
2022-05-13 19:50:22 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+new WOW().init();
+var typed = new Typed('.typing', {
+strings: ["Web Designer.", "Code Developer.","Graphic Designer."],
+typeSpeed:50,
+backSpeed:50,
+loop:true,
+});
